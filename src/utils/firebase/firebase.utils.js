b84a7be3ad2a74abedb1320fd862885e02817cdc
Elizabeth Joseph <lizzy22.mj@gmail.com>
2023-06-09 18:15:09 +0100

    Added a lot of components

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp} from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,17 +14,21 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
 	prompt: "select-account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+	if (!userAuth) return;
+
 	const userDocRef = doc(db,'users', userAuth.uid);
 
 	const userSnapshot = await getDoc(userDocRef);
@@ -35,7 +39,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
 		try {
 			await setDoc(userDocRef, {
-				displayName, email, createdAt
+				displayName, email, createdAt, ...additionalInformation
 			});
 		} catch (error) {
 			console.log('error creating the user', error.message);
@@ -43,4 +47,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 	}
 	
 	return userDocRef;
-}
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+	return await createUserWithEmailAndPassword(auth, email, password);
+};
