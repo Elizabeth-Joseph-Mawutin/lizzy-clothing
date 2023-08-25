434128c8ed87efbf0a9d78568bef9b701ae2eeb3
Elizabeth Joseph <lizzy22.mj@gmail.com>
2023-08-25 18:04:14 +0100

    new changes

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import {ReactComponent as ShoppingIcon} from '../../assets/ShoppingIcon'
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
 
